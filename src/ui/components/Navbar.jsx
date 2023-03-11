0a122d16107f1f553a43702cb5f37f56a7d8811c
Luis <luismrv6@gmail.com>
2023-03-11 11:31:23 -0500

    Actualización del Navbar

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -13,50 +13,55 @@ export const Navbar = () => {
 
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
-            <div className="container-fluid">
 
-                <Link
-                    className="navbar-brand"
-                    to="/"
-                >
-                    Asociaciones
-                </Link>
+            <Link
+                className="navbar-brand"
+                to="/"
+            >
+                Asociaciones
+            </Link>
 
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <div className="navbar-nav">
+            <div className="navbar-collapse">
+                <div className="navbar-nav">
 
-                        <NavLink
-                            className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-                            to="/marvel"
-                        >
-                            Marvel
-                        </NavLink>
+                    <NavLink
+                        className={({ isActive }) => `nav-item nav-link  ${isActive ? 'active' : ''}`}
+                        to="/marvel"
+                    >
+                        Marvel
+                    </NavLink>
 
-                        <NavLink
-                            className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-                            to="/dc"
-                        >
-                            DC
-                        </NavLink>
-                        <NavLink
-                            className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-                            to="/search"
-                        >
-                            Search
-                        </NavLink>
-                    </div>
+                    <NavLink
+                        className={({ isActive }) => `nav-item nav-link  ${isActive ? 'active' : ''}`}
+                        to="/dc"
+                    >
+                        DC
+                    </NavLink>
+
+                    <NavLink
+                        className={({ isActive }) => `nav-item nav-link  ${isActive ? 'active' : ''}`}
+                        to="/search"
+                    >
+                        Search
+                    </NavLink>
                 </div>
+            </div>
 
-                <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
-                    <ul className="navbar-nav ml-auto">
-                        <span className='nav-item nav-link text-primary'>
-                            Luis
-                        </span>
-                        <button className='nav-item nav-link btn' onClick={ onLogout }>
-                            Logout
-                        </button>
-                    </ul>
-                </div>
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+                <ul className="navbar-nav ml-auto">
+
+                    <span className="nav-item nav-link text-primary">
+                        Luis
+                    </span>
+
+                    <button
+                        className="nav-item nav-link btn"
+                        onClick={onLogout}
+                    >
+                        Logout
+                    </button>
+
+                </ul>
             </div>
         </nav>
     )
