2d7b4f8647b0737af695a01683ea7bcf1a499b0d
Luis <luismrv6@gmail.com>
2023-03-11 19:39:23 -0500

    Se agregó las rutas públicas.

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes';
 import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 
 export const AppRouter = () => {
@@ -10,11 +11,17 @@ export const AppRouter = () => {
     <>
       <Routes>
 
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        } />
 
-        <Route path="/*" element={<PrivateRoute>
-          <HeroesRoutes />
-        </PrivateRoute>}></Route>
+        <Route path="/*" element={
+          <PrivateRoute>
+            <HeroesRoutes />
+          </PrivateRoute>
+        } />
 
       </Routes>
     </>
